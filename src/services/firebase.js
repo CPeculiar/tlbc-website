8a450954a96e951ca896890f73104a6cde05dc96
CPeculiar <pecudamicable@gmail.com>
2024-11-12 18:04:15 +0100

    fixes 2

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -6,14 +6,24 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from "firebase/storage"; 
 
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+//   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+// };
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAK8LK8gJTIbhbiLb2TOzdt7V6V6rD73OQ",
+  authDomain: "tlbc-website-fea0c.firebaseapp.com",
+  projectId: "tlbc-website-fea0c",
+  storageBucket: "tlbc-website-fea0c.firebasestorage.app",
+  messagingSenderId: "752932440220",
+  appId: "1:752932440220:web:aadc71637e9ba25c5fc559",
+  measurementId: "G-F8DS6PH8KR"
 };
 
 const app = initializeApp(firebaseConfig);
